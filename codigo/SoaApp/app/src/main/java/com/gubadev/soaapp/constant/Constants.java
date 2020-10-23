36bb9139049597d773829979e1711a31eeb02bda
Guido_Barra <Guido_Barra@ITRP0478>
2020-10-23 20:54:38 -0300

    add constants

---
+++ codigo/SoaApp/app/src/main/java/com/gubadev/soaapp/constant/Constants.java
@@ -0,0 +1,31 @@
+package com.gubadev.soaapp.constant;
+
+public class Constants {
+
+    public static final String ENV_TEST = "TEST";
+    public static final String ENV_PROD = "PROD";
+    public static final String ENV = ENV_TEST;
+
+    public static final String URI_CATEDRA_SOA = "http://so-unlam.net.ar/api/api/";
+    public static final String URI_CATEDRA_SOA_REGISTER = "http://so-unlam.net.ar/api/api/register";
+    public static final String URI_CATEDRA_SOA_LOGIN = "http://so-unlam.net.ar/api/api/login";
+
+    public static final String STATE_SUCCESS = "success";
+    public static final String STATE_ERROR = "error";
+
+    public static final Long NUM_COMMISSION = 2L;
+    public static final Integer NUM_GROUP = 22;
+
+
+    public static final String TABLE_USER = "User";
+    public static final String EMAIL = "email";
+    public static final String FIRST_NAME = "first_name";
+    public static final String LAST_NAME = "last_name";
+    public static final String DNI = "dni";
+    public static final String PASSWORD = "password";
+
+    public static final String ID = "id";
+
+
+
+}
