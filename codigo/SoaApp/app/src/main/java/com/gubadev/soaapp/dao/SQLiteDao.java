5e374feb61ce3e92567585e5047c0a8db42a32e4
Guido_Barra <Guido_Barra@ITRP0478>
2020-10-27 22:40:06 -0300

    add description SQLiteDAO

--- codigo/SoaApp/app/src/main/java/com/gubadev/soaapp/dao/SQLiteDao.java
+++ codigo/SoaApp/app/src/main/java/com/gubadev/soaapp/dao/SQLiteDao.java
@@ -27,31 +27,41 @@ public class SQLiteDao {
     private static Cursor c;
 
 
+    /**
+     * BUILDER SQLiteDao
+     */
     public static SQLiteDatabase builder(Context context) {
         SQLiteConfig dbHelper = new SQLiteConfig(context);
         return dbHelper.getWritableDatabase();
     }
 
+    /**
+     * SAVE USER IN SQLITE
+     */
     public static void saveUser(UserDTO user, @Nullable SQLiteDatabase db) {
 
         try {
-
+            /*QUERY*/
             c = db.rawQuery("Select * from " + Constants.TABLE_USER +
                                 " WHERE " + Constants.EMAIL + " =  ?",
                                 new String[]{user.getEmail()});
 
+            /*CHECK EMAIL EXIST*/
             if (c.getCount() != 0) {
                 Log.e(TAG_DAO_SQLITE, "error: email exist");
                 return;
             }
 
+            /*SET VALUES*/
             ContentValues values = new ContentValues();
             values.put(Constants.FIRST_NAME, user.getFirstName());
             values.put(Constants.LAST_NAME, user.getLastName());
             values.put(Constants.DNI, user.getDni());
             values.put(Constants.EMAIL, user.getEmail());
             values.put(Constants.PASSWORD, user.getPassword());
-            Long result = db.insert(Constants.TABLE_USER, Constants.ID, values);
+
+            /*SAVE OR INSERT USER*/
+            long result = db.insert(Constants.TABLE_USER, Constants.ID, values);
 
             Log.i(TAG_DAO_SQLITE, "DAO SQLite result: " + result);
 
@@ -63,53 +73,93 @@ public class SQLiteDao {
         }
     }
 
+    /**
+     * SAVE SCORE IN SQLITE
+     */
     public static void saveScore(@Nullable SQLiteDatabase db) {
+        try {
+            int idUser = -1;
+            String firstName = "";
+            String lastName = "";
 
-        int idUser = -1;
-        String firstName = "";
-        String lastName = "";
+            /*QUERY*/
+            c = db.rawQuery("Select * from " + Constants.TABLE_USER +
+                            " WHERE " + Constants.EMAIL + " =  ?",
+                    new String[]{MySingleton.getInstance().getEmail()});
 
+            /*CHECK EXIST ELEMENT*/
+            if (c.moveToFirst()) {
+                /*GET USER*/
+                do {
+                    firstName = c.getString(c.getColumnIndex(Constants.FIRST_NAME));
+                    lastName = c.getString(c.getColumnIndex(Constants.LAST_NAME));
+                    idUser = c.getInt(c.getColumnIndex(Constants.ID));
+                } while (c.moveToNext());
+            }
 
-        c = db.rawQuery("Select * from " + Constants.TABLE_USER +
-                        " WHERE " + Constants.EMAIL + " =  ?",
-                new String[]{MySingleton.getInstance().getEmail()});
+            /*CHECK USER EXIST*/
+            if (idUser == -1 || firstName.isEmpty() || lastName.isEmpty()) {
+                Log.e(TAG_DAO_SQLITE, "error: user no exist");
+                return;
+            }
 
-        if (c.moveToFirst()){
-            do {
-                firstName = c.getString(c.getColumnIndex(Constants.FIRST_NAME));
-                lastName = c.getString(c.getColumnIndex(Constants.LAST_NAME));
-                idUser = c.getInt(c.getColumnIndex(Constants.ID));
-            } while(c.moveToNext());
+            /*GET DATE NOW*/
+            String pattern = "YYYY-MM-DD HH:MM:SS";
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern, Locale.getDefault());
+            String date = simpleDateFormat.format(new Date());
+
+            /*SET VALUES*/
+            ContentValues values = new ContentValues();
+            values.put(Constants.DATE, date);
+            values.put(Constants.SCORE, MySingleton.getInstance().getScore());
+            values.put(Constants.TIME, MySingleton.getInstance().getTime());
+            values.put(Constants.NAME_GAMER, firstName + "_" + lastName);
+            values.put(Constants.USER_ID, idUser);
+
+            /*SAVE OR INSERT SCORE*/
+            long result = db.insert(Constants.TABLE_SCORE, Constants.ID, values);
+
+            Log.i(TAG_DAO_SQLITE, "DAO SQLite result: " + result);
+        } catch (Exception e) {
+            Log.e(TAG_DAO_SQLITE, "DAO SQLite ERROR: " + e.getMessage(), e);
+        } finally {
+            c.close();
+            db.close();
         }
-
-
-        String pattern = "YYYY-MM-DD HH:MM:SS";
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern, Locale.getDefault());
-        String date = simpleDateFormat.format(new Date());
-
-        ContentValues values = new ContentValues();
-        values.put(Constants.DATE, date);
-        values.put(Constants.SCORE, MySingleton.getInstance().getScore());
-        values.put(Constants.TIME, MySingleton.getInstance().getTime());
-        values.put(Constants.NAME_GAMER, firstName + "_" + lastName);
-        values.put(Constants.USER_ID, idUser);
-        Long result = db.insert(Constants.TABLE_SCORE, Constants.ID, values);
     }
 
+    /**
+     * GET TOP GAMER OF SQLITE
+     */
     public static List<Score> getTopGamer(@Nullable SQLiteDatabase db) {
-
+        /*LIST OF SCORES*/
         List<Score> scores = new ArrayList<>();
-        c = db.rawQuery("Select * from " + Constants.TABLE_SCORE, null);
 
-        if (c.moveToFirst()){
-            do {
-                Score score = new Score(
-                        c.getInt(c.getColumnIndex(Constants.SCORE)),
-                        c.getInt(c.getColumnIndex(Constants.TIME)),
-                        c.getString(c.getColumnIndex(Constants.NAME_GAMER)),
-                        c.getString(c.getColumnIndex(Constants.DATE)));
-                scores.add(score);
-            } while(c.moveToNext());
+        try {
+
+
+            /*QUERY*/
+            c = db.rawQuery("Select * from " + Constants.TABLE_SCORE, null);
+
+            /*CHECK EXIST ELEMENT*/
+            if (c.moveToFirst()) {
+                do {
+                    /*INSTANCE SCORE*/
+                    Score score = new Score(
+                            c.getInt(c.getColumnIndex(Constants.SCORE)),
+                            c.getInt(c.getColumnIndex(Constants.TIME)),
+                            c.getString(c.getColumnIndex(Constants.NAME_GAMER)),
+                            c.getString(c.getColumnIndex(Constants.DATE)));
+
+                    /*ADD SCORE TO LIST*/
+                    scores.add(score);
+                } while (c.moveToNext());
+            }
+        } catch (Exception e) {
+            Log.e(TAG_DAO_SQLITE, "DAO SQLite ERROR: " + e.getMessage(), e);
+        } finally {
+            c.close();
+            db.close();
         }
 
         return scores;
