a6b691266462bae451d54e6f99c14bf06b8b28ba
Guido_Barra <Guido_Barra@ITRP0478>
2020-11-01 00:58:18 -0300

    Insert User

--- codigo/SoaApp/app/src/main/java/com/gubadev/soaapp/configuration/SQLiteConfig.java
+++ codigo/SoaApp/app/src/main/java/com/gubadev/soaapp/configuration/SQLiteConfig.java
@@ -27,8 +27,19 @@ public class SQLiteConfig extends SQLiteOpenHelper {
                                          "FOREIGN KEY (" + Constants.USER_ID + ") REFERENCES " +
                                           Constants.TABLE_USER + "(" + Constants.ID + "));");
 
+    private static final String INSERT_USER = ("INSERT INTO " + Constants.TABLE_USER + " (" +
+                                                Constants.EMAIL + ", " +
+                                                Constants.PASSWORD + ", " +
+                                                Constants.DNI + ", " +
+                                                Constants.FIRST_NAME + ", " +
+                                                Constants.FIRST_NAME + ") VALUES " +
+                                                "('guidoguba@gmail.com'  , 'guidoguba', 123342211, 'albert' , 'barra'   ), "+
+                                                "('testtest@test.com'    , 'testtest' , 123342212, 'nicolas', 'gonzales'), "+
+                                                "('testguido@hotmail.com', 'testguido', 123342213, 'jose'   , 'Skypo'   ), "+
+                                                "('guidotest@test.com'   , 'guidotest', 123342214, 'pepe'   , 'Ceilo'   );");
+
     private static final String DB_NAME = "db.sqlite";
-    private static final int DB_VERSION = 3;
+    private static final int DB_VERSION = 5;
     private static final String TAG_DB_SQLITE = "DB_SQLITE";
 
     public SQLiteConfig(Context context) {
@@ -41,6 +52,7 @@ public class SQLiteConfig extends SQLiteOpenHelper {
         Log.i(TAG_DB_SQLITE, "onCreate: CREATE TABLE");
         db.execSQL(USERS);
         db.execSQL(SCORE);
+        db.execSQL(INSERT_USER);
     }
 
     @Override
@@ -49,10 +61,12 @@ public class SQLiteConfig extends SQLiteOpenHelper {
         Log.i(TAG_DB_SQLITE, "onUpgrade: DROP TABLE");
         db.execSQL("DROP TABLE IF EXISTS " + Constants.TABLE_USER);
         db.execSQL("DROP TABLE IF EXISTS " + Constants.TABLE_SCORE);
+        onCreate(db);
     }
 
     @Override
     public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        onUpgrade(db, oldVersion, newVersion);
         db.setVersion(oldVersion);
     }
 
